5d0b169300b540d2294c1d9d10ea1222e217a249
Lasse Hallingskov <lass347s@stud.kea.dk>
2023-09-26 15:31:55 +0200

    Updated RestController with DTO logic and ResponseEntity.

--- src/main/java/dk/kea/jpastudentdat22c/controller/StudentRestController.java
+++ src/main/java/dk/kea/jpastudentdat22c/controller/StudentRestController.java
@@ -1,67 +1,61 @@
 package dk.kea.jpastudentdat22c.controller;
 
-import dk.kea.jpastudentdat22c.dto.StudentConverter;
 import dk.kea.jpastudentdat22c.dto.StudentDTO;
-import dk.kea.jpastudentdat22c.model.Student;
-import dk.kea.jpastudentdat22c.repository.StudentRepository;
+import dk.kea.jpastudentdat22c.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @CrossOrigin
 @RestController
 public class StudentRestController {
 
     @Autowired
-    StudentRepository studentRepository;
+    StudentService studentService;
 
-    @Autowired
-    StudentConverter studentConverter;
-
-    @GetMapping("/")
-    public List<StudentDTO> getAllStudents() {
-        List<Student> studentList = studentRepository.findAll();
-        List<StudentDTO> studentDTOList = new ArrayList<>();
-        studentList.forEach(s -> {
-            studentDTOList.add(studentConverter.studentToDTO(s));
-        });
-        for (Student s : studentList) {
-            studentDTOList.add(studentConverter.studentToDTO(s));
-        }
-        return studentDTOList;
+    @GetMapping("/students")
+    public ResponseEntity<List<StudentDTO>> getAllStudents() {
+        List<StudentDTO> studentDTOList = studentService.getAllStudents();
+        return new ResponseEntity<>(studentDTOList, HttpStatus.OK);
     }
-
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Student createStudent(@RequestBody StudentDTO studentDTO) {
-        Student student = studentConverter.toEntity(studentDTO);
-
-        student.setId(0);
-        System.out.println(student);
-        return (studentRepository.save(student));
+    public ResponseEntity<StudentDTO> createStudent(@RequestBody StudentDTO studentDTO) {
+        StudentDTO createdStudent = studentService.createStudent(studentDTO);
+        return new ResponseEntity<>(createdStudent, HttpStatus.CREATED);
     }
 
+    @GetMapping("/students/{id}")
+    public ResponseEntity<StudentDTO> getStudentById(@PathVariable("id") int id) {
+        StudentDTO studentDTO = studentService.getStudentById(id);
+        return ResponseEntity.ok(studentDTO);
+    }
+
+    @PutMapping("/students/{id}")
+    public ResponseEntity<StudentDTO> putStudent(@PathVariable int id, @RequestBody StudentDTO studentDTO) {
+        StudentDTO updatedStudentDTO = studentService.updateStudent(id, studentDTO);
+        return ResponseEntity.ok(updatedStudentDTO);
+    }
+
+    @DeleteMapping("/students/{id}")
+    public ResponseEntity<Void> deleteStudent(@PathVariable int id) {
+        studentService.deleteStudentById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+    }
+
+
+    /*
     @GetMapping("/students/{name}")
     public List<Student> getAllStudentsByName(@PathVariable String name) {
         return studentRepository.findAllByName(name);
     }
 
-    @GetMapping("/students/{id}")
-    public ResponseEntity<StudentDTO> getStudentById(@PathVariable int id) {
-        Optional<Student> optionalStudent = studentRepository.findById(id);
-        if (optionalStudent.isPresent()) {
-            return ResponseEntity.ok(studentConverter.studentToDTO(optionalStudent.get()));
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-    }
-/*
+
+
     @PutMapping("/students/{id}")
     public Student updateStudent(@PathVariable int id, @RequestBody Student student) {
         Student s = studentRepository.findById(id).get();
@@ -73,28 +67,4 @@ public class StudentRestController {
     }
 
  */
-
-    @PutMapping("/students/{id}")
-    public ResponseEntity<StudentDTO> putStudent(@PathVariable int id, @RequestBody StudentDTO studentDTO) {
-        Optional<Student> optionalStudent = studentRepository.findById(id);
-        if (optionalStudent.isPresent()) {
-            Student student = studentConverter.toEntity(studentDTO);
-            student.setId(id);
-            studentRepository.save(student);
-            return ResponseEntity.ok(studentConverter.studentToDTO(student));
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-    }
-
-    @DeleteMapping("/students/{id}")
-    public ResponseEntity<String> deleteStudent(@PathVariable int id) {
-        Optional<Student> optionalStudent = studentRepository.findById(id);
-        if (optionalStudent.isPresent()) {
-            studentRepository.delete(optionalStudent.get());
-            return ResponseEntity.status(HttpStatus.OK).body("Student with id " + id + " was deleted");
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student with id " + id + " was not found");
-        }
-    }
 }
